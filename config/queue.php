c31b16287789188280b6ce2bab8117a9ac8e2f55
Roshan Timalsena <roshan@bookmundi.com>
2025-06-22 15:05:49 +0545

    fix: updated queue config

--- config/queue.php
+++ config/queue.php
@@ -73,12 +73,11 @@ return [
 
         'rabbitmq' => [
             'driver' => 'rabbitmq',
-            'host' => env('RABBITMQ_HOST'),
-            'port' => env('RABBITMQ_PORT'),
-            'login' => env('RABBITMQ_LOGIN'),
-            'password' => env('RABBITMQ_PASSWORD'),
+            'host' => env('RABBITMQ_HOST', '127.0.0.1'),
+            'port' => env('RABBITMQ_PORT', 5672),
+            'login' => env('RABBITMQ_LOGIN', 'guest'),
+            'password' => env('RABBITMQ_PASSWORD', 'guest'),
         ],
-
     ],
 
     /*
